077c9d7dc0949d86528a3ee656b80293f95865d1
Yuri Stuken <stuken.yuri@gmail.com>
2015-02-09 14:30:33 +0300

    Activation functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import scipy.special
+
 from neural_network import *
 
 if __name__ == "__main__":
-    nn = NeuralNetwork([2, 10, 15, 2, 1])
+    nn = NeuralNetwork([2, 10, 15, 2, 1], 3*[numpy.tanh] + [scipy.special.expit])
 
     nn.activate(numpy.array([[1, 2], [2, 3], [3, 4]]))
--- neural_network.py
+++ neural_network.py
@@ -1,8 +1,9 @@
 import numpy
 
 class NeuralNetwork:
-    def __init__(self, dimensions):
+    def __init__(self, dimensions, activation_functions):
         self.dimensions = dimensions
+        self.activation_functions = activation_functions
         # self.hidden_layers_count = len(dimensions)
         self.weight_matrices = self.generate_random_weights()
         return
@@ -22,5 +23,5 @@ class NeuralNetwork:
             # add bias to the input
             bias_column = numpy.ones((current_value.shape[0], 1))
             current_value = numpy.concatenate((current_value, bias_column), axis=1)
-            current_value = current_value.dot(weights)
+            current_value = self.activation_functions[i](current_value.dot(weights))
             print "Layer " + str(i + 1) + ", Activation values: " + str(current_value)
